40d7eb90ca08f19488e129ce5deeec03832c665b
dzshzx <22311806+dzshzx@users.noreply.github.com>
2026-09-21 12:51:34 +0800

    chore(release): publish architecture improvements as patch updates

--- dist/feishu-preview-image-export.user.js
+++ dist/feishu-preview-image-export.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Feishu Preview Image Export
 // @namespace    https://github.com/dzshzx/custom-user-js-scripts
-// @version      0.1.3
+// @version      0.1.4
 // @description  Export the main visible image from a Feishu file preview page.
 // @author       dzshzx
 // @match        https://mi.feishu.cn/file/*
